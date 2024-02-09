9f6cf4895662792578b8556c39a9feaf0eabea35
Manuel Tavares <manueldiogotv@gmail.com>
2024-02-09 18:53:42 +0000

    in a way

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import { useState } from 'react'
-
-import "./components/Footer.jsx";
+import { Routes, Route } from 'react-router-dom';
 import Footer from "./components/Footer.jsx";
-
-import "./components/Navbar.jsx";
 import Navbar from "./components/Navbar.jsx";
-
-import "./components/Sidebar.jsx";
 import Sidebar from "./components/Sidebar.jsx";
-
 import List from './components/List.jsx';
-
-
+import About from "./pages/About.jsx"
 
 //import './App.css';
 
@@ -19,12 +12,13 @@ function App() {
 
   return (
     <div className='App'>
-      <Sidebar />
-      
+
       <Navbar />
-      
-      <List  />
-      
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<List />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
       <Footer />
     </div>
   )
